2cedeaf59f3fe3f53c63d5e5622545a75092d7f4
azizsheq <azizsheq@gmail.com>
2021-02-02 18:27:36 +0800

    updating true vs flase

---
+++ trueFalse.js
@@ -0,0 +1,35 @@
+const age = 0;
+// const age = 1;
+// in a true false condition if the value is zero the condition will be FALSE
+// but if the value is any other number except zero then the condition will be TRUE
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+// const name = "Salman";
+const name = "";
+// all string are TRUE except empty string such as "" but if the string having a empty space inside it then it's also TRUE
+// only empty string "" is FALSE in js
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// all the FALSY values are - 
+    // false
+    // 0
+    // ""
+    // undefined
+    // null
+    //NaN
+
+// all the TRUTHY values are - 
+    // '0'
+    // ' '
+    // []
+    // 'false'
+    // all other values except 0 or empty string
